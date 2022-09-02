452b3e8c5e040017f5febefb3b7feb735d38220b
tgabor1 <t.gabor@tutanota.com>
2022-09-02 12:25:55 +0200

    _

--- routes/hotels.js
+++ routes/hotels.js
@@ -6,8 +6,7 @@ const router = express.Router()
 
 // create
 router.post("/", async (req, res) => {
-
-const newHotel = new Hotel(req.body)
+    const newHotel = new Hotel(req.body)
 
     try {
         const savedHotel = await newHotel.save()
@@ -18,8 +17,43 @@ const newHotel = new Hotel(req.body)
 })
 
 // update
+router.put("/:id", async (req, res) => {
+    try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
+        res.status(200).json(updatedHotel)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 // delete
+router.delete("/:id", async (req, res) => {
+    try {
+        await Hotel.findByIdAndDelete(req.params.id)
+        res.status(200).json("Hotel has been deleted.")
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 // get
+router.get("/:id", async (req, res) => {
+    try {
+        const hotel = await Hotel.findById(req.params.id)
+        res.status(200).json(hotel)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 // get all
+router.get("/", async (req, res, next) => {
+    try {
+        const hotels = await Hotel.find()
+        res.status(200).json(hotels)
+    } catch (err) {
+        next(err)
+    }
+})
 
 export default router
